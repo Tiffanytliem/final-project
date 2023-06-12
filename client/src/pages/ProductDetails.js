4bace332a20e2b849e25c2096178b0e95b9d07d8
Tiffany Liem <thuytliem@gmail.com>
2023-06-12 08:03:03 +0000

    Styling in progress

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { fetchProduct } from '../lib/api';
-import {Link, useParams} from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
 import '../styles.css';
 
 export default function ProductDetails() {
-  const {productId} = useParams();
+  const { productId } = useParams();
   console.log(useParams());
   console.log(productId);
   const [product, setProduct] = useState();
@@ -35,22 +35,41 @@ export default function ProductDetails() {
     );
   }
   if (!product) return null;
-  const { name, images, price, description} = product;
+  const { name, images, price, description } = product;
   return (
     <div className="container">
-      <div className="card shadow-sm">
-        <div className="card-body">
+      <div className="row">
+        <div className="img-col-2-thrd">
           <div className="row">
+            <img src={images[0]} alt={name} className="image" />
+          </div>
+          {/* <div className="row">
+            <div className="img-col-1-fifth">
+              <img src={images[1]} alt={name} className="image" />
+            </div>
+            <div className="img-col-1-fifth">
+              <img src={images[2]} alt={name} className="image" />
+            </div>
+            <div className="img-col-1-fifth">
+              <img src={images[3]} alt={name} className="image" />
+            </div>
+            <div className="img-col-1-fifth">
+              <img src={images[4]} alt={name} className="image" />
+            </div>
+          </div> */}
+        </div>
+        <div className="desc-col-1-thrd">
+          <div className="row">
+            <h2>{name}</h2>
           </div>
           <div className="row">
-            <div className="column-half">
-              <img src={images[0]} alt={name} className="image" />
-            </div>
-            <div className="column-half">
-              <h2>{name}</h2>
-              <h4 className="text-secondary">{price}</h4>
-              <p>{description}</p>
-            </div>
+            <p className="detail-price">{price}</p>
+          </div>
+          <div className="row">
+            <button className="add-to-cart">Add to Cart</button>
+          </div>
+          <div className="row">
+            <p className="detail-desc">{description}</p>
           </div>
         </div>
       </div>
